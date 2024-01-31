a95719df4c8b694e9cdf5de90b11e04ed4ab9385
Seojisoo20191941 <amy0480@naver.com>
2024-01-31 15:41:20 +0900

    P43162

--- algorithm/P43162.py
+++ algorithm/P43162.py
@@ -1,32 +1,39 @@
 from collections import deque
 
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
 
 def solution(n, computers):
+    def bfs(i, j):
+        q=deque()
+        computers[i][j]=0
+        computers[i][i]=0
+        computers[j][j]=0
+        q.append([i, j])
+
+        result=0
+
+        while q:
+            x, y=q.popleft()
+            for a in range(n):
+                if a!=y and computers[y][a]==1:
+                    computers[y][a]=0
+                    computers[y][y]=0
+                    computers[a][a]=0
+                    q.append([y, a])
+
+        result+=1
+
+        return result
+    
     answer = 0
     
     for i in range(n):
         for j in range(n):
+            if i!=j and computers[i][j]==1:
+                answer+=bfs(i, j)
+            
+    for i in range(n):
+        for j in range(n):
             if computers[i][j]==1:
-                q=deque()
-                q.append([i, j])
-                computers[i][j]=0
-                
-                while q:
-                    x, y=q.popleft()
-                    for k in range(4):
-                        nx=x+dx[k]
-                        ny=y+dy[k]
-
-                        if 0<=nx<n and 0<=ny<n:
-                            if computers[nx][ny]==1:
-                                computers[nx][ny]=0
-                                q.append([nx, ny])
-                
                 answer+=1
                 
-    print(computers)
     return answer
-
-solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])
